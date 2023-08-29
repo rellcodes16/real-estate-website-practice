4e303f3ae7841003fa5bd1c1594e5cb001a8096e
JustRell <devrellmara@gmail.com>
2023-08-29 08:57:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Companies from "./Component/Companies/Companies";
+import Contact from "./Component/Contact/Contact";
+import Footer from "./Component/Footer/Footer";
+import GetStarted from "./Component/GetStarted/GetStarted";
+import Header from "./Component/Header/Header";
+import Hero from "./Component/Hero/Hero";
+import Residencies from "./Component/Residencies/Residencies";
+import Value from "./Component/Value/Value";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+
